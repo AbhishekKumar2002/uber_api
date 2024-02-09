255c8d8c381abb72df47ecf533fe482732ec8c9c
Abhishek Kumar <96721263+AbhishekKumar2002@users.noreply.github.com>
2024-02-09 13:53:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 import pickle
 import pandas as pd
 import numpy as np
+import os
 #import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
