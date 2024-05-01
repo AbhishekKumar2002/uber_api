0d5823990905be8b31e6fe0adf9b82e7945e3eb5
Abhishek Kumar <96721263+AbhishekKumar2002@users.noreply.github.com>
2024-05-01 21:57:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ if __name__ == "__main__":
      # Use the port provided by Render
     port = int(os.environ.get("PORT", 8000))
     # Listen on all network interfaces
-    uvicorn.run(app, host="127.0.0.1", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
